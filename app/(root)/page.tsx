e9885a221783f2618069d5b8ab7744cde195ce11
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-03-24 20:27:44 +0530

    right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,29 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = {firstName:'Rajasva Raj'}
+    const loggedIn = { firstName: 'Rajasva Raj' }
     return (
         <section className='home'>
             <div className='home-content'>
                 <header className='home-header'>
-                   <HeaderBox 
-                   type='greeting'
-                   title='Welcome to Banking App'
-                   user={loggedIn?.firstName || 'Guest'}
-                   subtext='Access and manage your account and transactions efficiently'
-                   />
-                   <TotalBalanceBox
-                    accounts={[]}
-                    totalBanks={1}
-                    totalCurrentBalance={1250.35}
-                   />
+                    <HeaderBox
+                        type='greeting'
+                        title='Welcome to Banking App'
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext='Access and manage your account and transactions efficiently'
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
                 </header>
+                recent  tranction
             </div>
+            <RightSideBar />
         </section>
     )
 }
